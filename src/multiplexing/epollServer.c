cdbdefc41b2ab8f70ae9325f878d8a3d7d2ec1c0
lincon <shizhandev@gmail.com>
2021-03-03 20:49:13 +0800

    增加epoll、poll和操作系统判断

--- src/multiplexing/epollServer.c
+++ src/multiplexing/epollServer.c
@@ -1,31 +1,22 @@
 //
 // Created by Lincoln
 //
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <netinet/in.h>
 #include <fcntl.h>
-#include <poll.h>
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/epoll.h>
 
 #pragma clang diagnostic push
 #pragma ide diagnostic ignored "EndlessLoop"
 
-#define MAX_CONNECTION 1024
-
-/**
- * 没有1024限制
- * poll的时候需要传递到内核
- *需要遍历所有的客户端,来确定哪些发生了变化,大量并发，少量活跃的时候，效率不高
- * @param argc
- * @param argv
- * @return
- */
-int main(int argc, char *argv[]) {
-    //初始化 服务器端socket
+int main() {
+//初始化 服务器端socket
     int listenFD = socket(AF_INET, SOCK_STREAM, 0);
 
     // 初始化socket的地址
@@ -52,79 +43,89 @@ int main(int argc, char *argv[]) {
     }
 
     //监听端口：修改lfd为被动socket，20是backlog的大小
-    listen(listenFD, 20);
-
-    printf("poll.开始接收连接....listenFD:%d\n", listenFD);
-
-    struct pollfd client[MAX_CONNECTION]; //最大打开的连接
-    for (int i = 1; i < MAX_CONNECTION; i++) {
-        client[i].fd = -1;
+    if (listen(listenFD, 20) < 0) {
+        perror("listen失败");
+        exit(1);
     }
 
-    client[0].fd = listenFD;
-    client[0].events = POLLIN;
-    int maxIdx = 0; //client数组的最大下标
+    printf("epoll.开始接收连接....listenFD:%d\n", listenFD);
 
+    //创建epoll实例,size:需要内核检测的描述符数量， Linux 2.6.8 参数被忽略,内核会动态分配
+    int epollFD = epoll_create(1);
+    if (epollFD == -1) {
+        perror("创建epoll实例失败\n");
+        exit(1);
+    }
+
+    const int MAX_EVENT = 1024;
+    struct epoll_event event;
+    struct epoll_event *events = malloc(sizeof(event) * MAX_EVENT);
+
+    event.data.fd = listenFD;
+    event.events = EPOLLIN;
+
+    //添加‘监听套接字‘到epoll实例
+    int res = epoll_ctl(epollFD, EPOLL_CTL_ADD, listenFD, &event);
+    if (res == -1) {
+        perror("添加listenFD到epoll失败");
+        exit(1);
+    }
 
     while (1) {
-        int changeFDNum = poll(client, maxIdx + 1, -1);
-        printf("poll触发,变化的FD个数:%d\n", changeFDNum);
-        if (changeFDNum < 0) { //变化的个数小于0
-            perror("poll error");
-            continue;
-        }
-        for (int i = 0; i <= maxIdx; i++) {
-            if (client[i].fd < 0) { //这个位置没有描述符
+        //阻塞等待
+        int changeNum = epoll_wait(epollFD, events, MAX_EVENT, -1);
+        printf("epll_wait返回,变化的描述符数:%d\n", changeNum);
+        for (int i = 0; i < changeNum; i++) {
+            if (!(events[i].events & EPOLLIN)) { //只处理可读事件
                 continue;
             }
-            if (!(client[i].revents & POLLIN)) { //只处理可读事件
-                continue;
-            }
-            if (client[i].fd == listenFD) { //监听套接字，有可读事件
+            if (events[i].data.fd == listenFD) { //监听套接字可读，说明有新的连接
                 // 接收客户端连接
                 struct sockaddr_in cliaddr;
                 socklen_t cliaddr_len = sizeof(cliaddr);
                 int connfd = accept(listenFD, (struct sockaddr *) &cliaddr, &cliaddr_len);
+                if (connfd < 0) {
+                    perror("accept失败");
+                    continue;
+                }
                 char str[INET_ADDRSTRLEN];
                 printf("接收到连接 IP:%s,PORT:%d,curFD:%d\n",
                        inet_ntop(AF_INET, &cliaddr.sin_addr, str, sizeof(str)),
                        ntohs(cliaddr.sin_port), connfd);
-                int curIndx = 1;
-                for (curIndx = 1; curIndx < MAX_CONNECTION; curIndx++) {
-                    if (client[curIndx].fd < 0) { //找到空闲的位置
-                        break;
-                    }
-                }
-                if (curIndx == MAX_CONNECTION) {
-                    printf("超过最大连接数\n");
-                    close(connfd);
-                    break;
-                }
-
                 //设置 客户端连接为 non-blocking
                 int flagOfConnfd = fcntl(connfd, F_GETFL);
                 flagOfConnfd |= O_NONBLOCK;
                 ret = fcntl(connfd, F_SETFL, flagOfConnfd);
                 if (ret < 0) {
                     perror("设置connFD非阻塞失败");
-                    exit(1);
                 }
-                client[curIndx].fd = connfd;
-                client[curIndx].events = POLLIN;
-                if (curIndx > maxIdx) {
-                    maxIdx = curIndx;
+                event.data.fd = connfd;
+                event.events = EPOLLIN;
+                if (epoll_ctl(epollFD, EPOLL_CTL_ADD, connfd, &event) < 0) {
+                    perror("添加connFD到epoll失败");
+                    close(connfd);
+                    continue;
                 }
-            } else { // 非连接描述符的可读事件
-                int curFD = client[i].fd;
+            } else {
+                int curFD = events[i].data.fd;
                 char buf[1024] = "";
                 int readBytes = read(curFD, buf, 1024);
                 if (readBytes < 0) {
                     if (EWOULDBLOCK == errno || EAGAIN == errno) {//非阻塞IO，缓冲区没数据
                         printf("缓冲区没有数据.continue. errno:%d\n", errno);
                         continue;
+                    } else {
+                        printf("数据读取失败,关闭连接\n");
+                        if (epoll_ctl(epollFD, EPOLL_CTL_DEL, curFD, NULL) < 0) {
+                            perror("从epoll删除FD失败");
+                        }
+                        close(curFD);
+                        printf("连接关闭.curFD:%d\n", curFD);
                     }
                 } else if (readBytes == 0) {//客户端关闭连接
-                    client[i].fd = -1;
+                    if (epoll_ctl(epollFD, EPOLL_CTL_DEL, curFD, NULL) < 0) {
+                        perror("从epoll删除FD失败");
+                    }
                     close(curFD);
                     printf("连接关闭.curFD:%d\n", curFD);
                 } else {//接收到数据，写入客户端
@@ -132,8 +133,10 @@ int main(int argc, char *argv[]) {
                 }
             }
         }
-
     }
+
+    free(events);
+    close(listenFD);
     return 0;
 }
 
